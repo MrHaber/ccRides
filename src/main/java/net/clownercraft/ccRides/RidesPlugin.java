48c344ade2a97f9469b4c841e13a7e1c6f7b8474
Nat Green <me@natgreen.co.uk>
2019-09-15 13:55:07 +0100

    Initial Commit

---
+++ src/main/java/net/clownercraft/ccRides/RidesPlugin.java
@@ -0,0 +1,16 @@
+package net.clownercraft.ccRides;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RidesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //TODO
+    }
+
+    @Override
+    public void onDisable() {
+        //TODO
+    }
+}
