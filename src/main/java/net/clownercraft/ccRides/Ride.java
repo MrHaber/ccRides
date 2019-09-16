1530bdca7ac732e06a2eb6e39d459587e75ca910
Nat Green <me@natgreen.co.uk>
2019-09-16 23:00:58 +0100

    Started Implementing Ride Functionality for Carousel

--- src/main/java/net/clownercraft/ccRides/Ride.java
+++ src/main/java/net/clownercraft/ccRides/Ride.java
@@ -1,10 +1,24 @@
 package net.clownercraft.ccRides;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Represents a generic single ride.
  */
 public abstract class Ride {
+    public int capacity = 0; //The number of available seats
+    public List<Vehicle> seats = new ArrayList<Vehicle>(); //stores vehicle entities for the seats
 
+    public Location base; //Centre location for the ride layout
+    public boolean rideRunning = false;
+
+    public List<Player> riders = new ArrayList<Player>();
 
     /**
      * Starts the movement of the ride.
@@ -16,5 +30,33 @@ public abstract class Ride {
      */
     public abstract void stopRide();
 
+    /**
+     * Triggers the ride to set the position of all it's vehicles,
+     */
+    public abstract void tickPositions();
+
+    /**
+     * Gets the location of a single seat of the ride
+     * @param seatNum
+     * @return the current location the seat should be in
+     */
+    public abstract Location getPosition(int seatNum);
+
+    /**
+     * Forces the ride to remove all it's vehicles/seats and respawn them.
+     * Should also re-seat all players in their vehicle after they are respawned
+     * May be triggered before a worldsave to prevent extra vehicles saving to world.
+     */
+    public abstract void respawnSeats();
+
+    /**
+     * despawns all this rides minecarts, to prevent duplicates appearing after a shutdown
+     */
+    public void despawnSeats() {
+        for (Vehicle v : seats) {
+            v.remove();
+        }
+        seats.clear();
+    }
 
 }
--- src/main/java/net/clownercraft/ccRides/rides/Carousel.java
+++ src/main/java/net/clownercraft/ccRides/rides/Carousel.java
@@ -1,14 +1,73 @@
 package net.clownercraft.ccRides.rides;
 
 import net.clownercraft.ccRides.Ride;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.util.Vector;
 
 public class Carousel extends Ride {
+    int radius;//the radius of the carousel seats
+    double currentRotation = 0.0; //Current rotation of the carousel in radians
+    int speed; //number of ticks per full rotation of the carousel
+    int length; //number of full rotations per ride.
+
+    public Carousel(Location loc, int cap, int radius, int speed, int length) {
+        super.capacity = cap;
+        super.base = loc;
+        this.radius = radius;
+        this.speed = speed;
+        this.length = length;
+
+        //Spawn a minecart for each seat of the ride.
+        for (int i=0;i<cap;i++) {
+            Location loc2 = getPosition(i);
+            Vehicle cart = (Vehicle) loc2.getWorld().spawnEntity(loc2, EntityType.MINECART);
+            //Make carts invulnerable, not affected by gravity and have no velocity
+            cart.setInvulnerable(true);
+            cart.setGravity(false);
+            cart.setVelocity(new Vector(0,0,0));
+            super.seats.add(cart);
+        }
+    }
 
     public void startRide() {
+        rideRunning = true;
         //TODO
     }
 
     public void stopRide() {
+        currentRotation = 0.0;
+        rideRunning = false;
         //TODO
     }
+
+    public void tickPositions() {
+        //TODO
+    }
+
+    public void respawnSeats() {
+        //despawn minecarts & clear seats list
+        super.despawnSeats();
+        //Spawn a minecart for each seat of the ride.
+        for (int i=0;i<capacity;i++) {
+            Location loc2 = getPosition(i);
+            Vehicle cart = (Vehicle) loc2.getWorld().spawnEntity(loc2, EntityType.MINECART);
+            //Make carts invulnerable, not affected by gravity and have no velocity
+            cart.setInvulnerable(true);
+            cart.setGravity(false);
+            cart.setVelocity(new Vector(0,0,0));
+            seats.add(cart);
+        }
+    }
+
+    /**
+     * gets the position of a seat
+     * @param seatNum = the index of the seat to check
+     * @return = the location the seat should currently be
+     */
+    public Location getPosition(int seatNum) {
+        return null;
+    }
+
 }
