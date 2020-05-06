c31b79ff513b660053c94532b0b825fc93ac20e6
N1ghthauq <39838106+N1ghthauq@users.noreply.github.com>
2020-05-06 13:35:08 -0500

    Working on CommandExecutor trying to get the command to run smoothly.

--- src/main/java/net/clownercraft/ccRides/CommandExecutor.java
+++ src/main/java/net/clownercraft/ccRides/CommandExecutor.java
@@ -13,76 +13,93 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String string, String[] argument) {
 
-        if(command.toString().equalsIgnoreCase("ccrides")) {
+        if(string.toString().equalsIgnoreCase("ccrides")) {
 
-            if(string.equalsIgnoreCase("config")) {
+            try{
 
-                //error detection: making sure there is a minimum number of arguments
-                if(argument.length < 4) {
+                if(argument[0].equalsIgnoreCase("help")){
 
-                    commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aThere must be at least 4 arguments for the config command!"));
-                    return true;
+                    return false;
 
                 }
 
-                //getting the enum type
-                switch(argument[1].toLowerCase()) {
+                if(argument[0].equalsIgnoreCase("config")) {
 
-                    case "information":
-                        type = ConfigHandler.ConfigType.INFORMATION;
-                        break;
-                    case "carousel":
-                        type = ConfigHandler.ConfigType.CAROUSEL;
-                        break;
-                    case "droptower":
-                        type = ConfigHandler.ConfigType.DROPTOWER;
-                        break;
-                    case "ferriswheel":
-                        type = ConfigHandler.ConfigType.FERRISWHEEL;
-                        break;
+                    //error detection: making sure there is a minimum number of arguments
+                    if(argument.length < 4) {
 
-                    default:
-                        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aSelect a config file to access!"));
+                        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@cThere must be at least 4 arguments for the config command!"));
+                        return false;
+
+                    }
+
+                    //getting the enum type
+                    switch(argument[1].toLowerCase()) {
+
+                        case "information":
+                            type = ConfigHandler.ConfigType.INFORMATION;
+                            break;
+                        case "carousel":
+                            type = ConfigHandler.ConfigType.CAROUSEL;
+                            break;
+                        case "droptower":
+                            type = ConfigHandler.ConfigType.DROPTOWER;
+                            break;
+                        case "ferriswheel":
+                            type = ConfigHandler.ConfigType.FERRISWHEEL;
+                            break;
+
+                        default:
+                            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aSelect a config file to access!"));
+                            return true;
+                    }
+
+                    //error detection: testing if config path exists
+                    String oldEntry = configHandler.getConfigPath(type, argument[3]);
+                    if(oldEntry == null) {
+
+                        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aThat config path doesn't exist for the selected config file!"));
                         return true;
-                }
 
-                //error detection: testing if config path exists
-                String oldEntry = configHandler.getConfigPath(type, argument[3]);
-                if(oldEntry == null) {
+                    }
 
-                    commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aThat config path doesn't exist for the selected config file!"));
-                    return true;
+                    switch(argument[2]){
 
-                }
+                        case "set":
 
-                switch(argument[2]){
-
-                    case "set":
-
-                        StringBuilder newEntry;
-                        newEntry = new StringBuilder("\"");
-                        int argumentLength = argument.length;
-                        for(int i = 2; i <= argumentLength; ++i) {
+                            StringBuilder newEntry;
+                            newEntry = new StringBuilder("\"");
+                            int argumentLength = argument.length;
+                            for(int i = 2; i <= argumentLength; ++i) {
                                 newEntry.append(" ").append(argument[i]);
-                        }
-                        newEntry.append('"');
-                        configHandler.setConfigPath(type, argument[3], newEntry.toString());
-                        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@2You have successfully set @d" + argument[3] + "@2 in @b" + argument[2] + "@2 from @6" + oldEntry + "@2 to @e" + newEntry + "@2!"));
-                        return true;
+                            }
+                            newEntry.append('"');
+                            configHandler.setConfigPath(type, argument[3], newEntry.toString());
+                            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@2You have successfully set @d" + argument[3] + "@2 in @b" + argument[2] + "@2 from @6" + oldEntry + "@2 to @e" + newEntry + "@2!"));
+                            return true;
 
-                    case "get":
+                        case "get":
 
-                        String entry = configHandler.getConfigPath(type, argument[3]);
-                        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@2The current entry in @b" + argument[2] + "@2 under @d" +argument[3] + "@2 is @6" + entry + "@2!"));
-                        return true;
+                            String entry = configHandler.getConfigPath(type, argument[3]);
+                            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@2The current entry in @b" + argument[2] + "@2 under @d" +argument[3] + "@2 is @6" + entry + "@2!"));
+                            return true;
 
-                    default:
-                        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aYou must choose between 'set' or 'get'!"));
-                        return true;
+                        default:
+                            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@aYou must choose between 'set' or 'get'!"));
+                            return false;
+
+                    }
 
                 }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+                commandSender.sendMessage(ChatColor.translateAlternateColorCodes('@', "@cYou must include an argument!"));
+                return false;
 
             }
+
+            return false;
 
         }
 
